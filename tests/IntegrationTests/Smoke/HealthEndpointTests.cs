0f3696dfaf3d00901fb091bbf7cd17772405dd81
Mahmoud Sallam <m.sallam@qoad.com>
2026-05-30 17:59:25 +0300

    push test

--- tests/IntegrationTests/Smoke/HealthEndpointTests.cs
+++ tests/IntegrationTests/Smoke/HealthEndpointTests.cs
@@ -3,16 +3,16 @@ using Xunit;
 
 namespace IntegrationTests.Smoke;
 
-//public class HealthEndpointTests : IClassFixture<EmmWebApplicationFactory>
-//{
-//    private readonly EmmWebApplicationFactory _factory;
-//    public HealthEndpointTests(EmmWebApplicationFactory factory) => _factory = factory;
+public class HealthEndpointTests : IClassFixture<EmmWebApplicationFactory>
+{
+    private readonly EmmWebApplicationFactory _factory;
+    public HealthEndpointTests(EmmWebApplicationFactory factory) => _factory = factory;
 
-//    [Fact]
-//    public async Task Health_endpoint_returns_OK()
-//    {
-//        var client = _factory.CreateClient();
-//        var response = await client.GetAsync(new Uri("/health", UriKind.Relative));
-//        response.IsSuccessStatusCode.Should().BeTrue();
-//    }
-//}
+    [Fact]
+    public async Task Health_endpoint_returns_OK()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync(new Uri("/health", UriKind.Relative));
+        response.IsSuccessStatusCode.Should().BeTrue();
+    }
+}
